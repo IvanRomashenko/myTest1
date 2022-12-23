84ce1d0be0a5a16e60d53c97216b1fa0c2f13445
IvanRomashenko <romashenko.smart@gmail.com>
2022-12-23 14:27:45 +0300

    cheng

--- src/myTest.java
+++ src/myTest.java
@@ -1,7 +1,8 @@
 public class myTest {
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
-            System.out.println("Hello world!!!");
+            int x = 100;
+            System.out.println("Hello world!!! " + x);
         }
     }
 }
