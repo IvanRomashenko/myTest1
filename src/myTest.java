60755867cc1fe7754f1cb94234b80100157f1a4a
IvanRomashenko <117081384+IvanRomashenko@users.noreply.github.com>
2022-12-23 15:49:06 +0300

    Delete myTest.java

--- src/myTest.java
+++
@@ -1,8 +0,0 @@
-public class myTest {
-    public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
-            int x = 100;
-            System.out.println("Hello world!!! " + x);
-        }
-    }
-}
